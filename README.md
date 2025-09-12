748aa6db4253765bd60cb5c855ef2984775c9928
Jeremy ANDRIAMBOLISOA <jeremy.andriambolisoa@piktura.fr>
2025-09-12 15:10:06 +0200

    feat: Created the rez package.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # TheKeyMachine-rez
+
+TheKeyMachine (TKM) is an open source set of tools designed for Maya animators.
+These tools are crafted to accelerate and facilitate many of the tasks animators undertake on a daily basis.
+
+## Requirements
+- Maya 2022+
+
+## Launch the tool in Maya
+Run this command :
+`import TheKeyMachine; TheKeyMachine.toggle()`
---
+++ package.py
@@ -0,0 +1,26 @@
+name = "TheKeyMachine"
+
+version = "0.1.41"
+
+authors = [
+    "Rodrigo Torres",
+    "Jeremy Andriambolisoa"
+]
+
+description = \
+    """
+    TheKeyMachine (TKM) is an open source set of tools designed for Maya animators.
+    These tools are crafted to accelerate and facilitate many of the tasks animators undertake on a daily basis.
+    """
+
+requires = [
+    "python-3+",
+    "maya-2022+",
+]
+
+uuid = "TheKeyMachine.TheKeyMachine"
+
+build_command = 'python {root}/build.py {install}'
+
+def commands():
+    env.PYTHONPATH.append("{root}/python")
