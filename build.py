7e41dd8b53fe5a0738960ed6375235a627c492bd
Jeremy ANDRIAMBOLISOA <jeremy.andriambolisoa@piktura.fr>
2025-09-30 18:07:11 +0200

    fix: modify the config file for studio installation.

--- build.py
+++ build.py
@@ -1,10 +1,38 @@
 import os
 import os.path
+import json
 import shutil
 import sys
 
+CONFIG_FILENAME = "config.json"
 
 def build(source_path, build_path, install_path, targets):
+
+    def write_config_file():
+        datas = {
+            "STUDIO_INSTALL": True,
+            "INSTALL_PATH": os.path.join(build_path, "python"),
+            "USER_FOLDER_PATH": "",
+            "LICENSE_FOLDER": "",
+            "LICENSE_FILE_NAME": "",
+            "UPDATER": False,
+            "BUG_REPORT": True,
+            "CUSTOM_TOOLS_MENU": True,
+            "CUSTOM_SCRIPTS_MENU": True,
+            "CUSTOM_TOOLS_EDITABLE_BY_USER": True,
+            "CUSTOM_SCRIPTS_EDITABLE_BY_USER": True
+        }
+
+        directory = os.path.join(
+            build_path,
+            "python",
+            "TheKeyMachine",
+            "data",
+            "config"
+        )
+
+        with open(os.path.join(directory, CONFIG_FILENAME), "w") as file:
+            file.write(json.dumps(datas, indent=4))
 
     def _build():
         # python source
@@ -13,6 +41,8 @@ def build(source_path, build_path, install_path, targets):
 
         if not os.path.exists(dest_py):
             shutil.copytree(src_py, dest_py)
+
+        write_config_file()
 
     def _install():
         src = os.path.join(build_path, "python")
